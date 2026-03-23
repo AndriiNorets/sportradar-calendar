713c00fa9e65eb585b2c3c4fa1f39e46aee093a0
AndriiNorets <hromnzlmng@gmail.com>
2026-03-23 23:00:26 +0100

    FIx migration file with format

--- alembic/versions/9384f3370321_create_initial_tables_q_q_esf_q_exit_e_q.py
+++ alembic/versions/9384f3370321_create_initial_tables_q_q_esf_q_exit_e_q.py
@@ -19,17 +19,17 @@ Create Date: 2026-03-23 21:44:37.159050
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "9384f3370321"
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
